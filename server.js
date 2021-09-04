13340acbd0ac3e26f6033a7286fff3846714db43
BeyYT <60990459+BeyYT@users.noreply.github.com>
2021-09-04 14:04:52 -0600

    jh

---
+++ server.js
@@ -0,0 +1,21 @@
+cors = require("cors");
+express = require("express");
+app = express();
+fs = require('fs')
+global.__basedir = __dirname;
+
+var corsOptions = {
+  origin: "http://localhost:" + process.env.PORT
+};
+
+app.use(cors(corsOptions));
+
+const initRoutes = require("./src/routes");
+
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+
+let port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Running at localhost:${port}`);
+});
