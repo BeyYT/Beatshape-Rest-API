cc5e7bea87be5d9673906e4c154d8edcfd565812
BeyYT <60990459+BeyYT@users.noreply.github.com>
2021-11-01 15:35:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+// Declaring Variables
 cors = require("cors");
 express = require("express");
 app = express();
 fs = require('fs')
 global.__basedir = __dirname;
 
+// In Case Of Use Of CORS
 var corsOptions = {
   origin: "http://localhost:" + process.env.PORT
 };
 
 app.use(cors(corsOptions));
 
+// App Routes
 const initRoutes = require("./src/routes");
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,5 +20,5 @@ initRoutes(app);
 
 let port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`Running atyfg localhost:${port}`);
+  console.log(`Running Beatshape API At: localhost:${port}`);
 });
